8c44180424b0e69b8a06a757c68d5c6029013439
anotherrealm <cecilkioko27@gmail.com>
2025-11-22 22:12:24 +0300

    feat: implement custom User model and configure AUTH_USER_MODEL

--- apps/movies/apps.py
+++ apps/movies/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 
 class MoviesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.movies'
-
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.movies"
+    label = "movies"
